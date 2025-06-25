865e8b7e337dd4886ae03f6166b5692cf756803a
sushitrash503 <crgraft503@gmail.com>
2025-06-25 16:13:56 -0600

    Add sitemap integration and create sitemap files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap'; // <-- nuevo
 
 // https://astro.build/config
 export default defineConfig({
-     integrations: [tailwind()],
+  site: 'https://bydaniel.site', // <-- MUY IMPORTANTE para el sitemap
+  integrations: [tailwind(), sitemap()], // <-- agrega el plugin aquí
 });
